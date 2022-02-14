57063917d23fade5d6a0491d132acbcb414e7414
Petri Koskinen <petri.koskinen@jkkone.fi>
2022-02-14 11:50:28 +0200

    Add project files.

---
+++ HeidenhainPlcLinenemberer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+var arguments = Environment.GetCommandLineArgs();
+if (arguments.Length < 2 || arguments.Length > 2)
+{
+    Console.WriteLine("This program takes exactly one argument: [filename]");
+    return;
+}
+    
+
+var filename = arguments[1];
+
+Console.WriteLine(filename);
+
+var sr = File.OpenText(filename);
+
+string? s;
+var counter = 0;
+var sb = new StringBuilder();
+
+while ((s = sr.ReadLine()) != null)
+{
+    if (s.Length > 4)
+    {
+        s = s.TrimStart(' '); // Remove whitespaces first
+        s = s.TrimStart('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'); // Then remove line numbers
+    }
+
+    // Substitute new line numbers
+    var fill = new string(' ', 5 - Convert.ToInt32(counter.ToString().Length)); 
+    var lineOutput = $"{fill}{counter++}{s}\n";
+    Console.Write(lineOutput);
+    sb.Append(lineOutput);
+}
+
+if (File.Exists(filename + ".numbered"))
+{
+    File.Delete(filename + ".numbered");
+}
+sr.Close();
+Console.WriteLine();
+var sw = File.AppendText(filename + ".numbered");
+sw.Write(sb);
+
+sw.Close();
